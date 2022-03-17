988ff6954ba652505ffc7bbdb508849f7374c73e
James Newell <james@jameslnewell.dev>
2022-03-17 23:37:40 +1100

    test

--- agent/docker.sh
+++ agent/docker.sh
@@ -3,6 +3,7 @@
 cd ./agent || exit
 docker build -t bk-agent  .
 docker run \
+  -e BUILDKITE_AGENT_TOKEN \
   -v "$(pwd)/hooks/environment:/buildkite/hooks/environment" \
   -v "${HOME}/.ssh/github_id_rsa:/buildkite-secrets/id_rsa_buildkite_git" \
   bk-agent
