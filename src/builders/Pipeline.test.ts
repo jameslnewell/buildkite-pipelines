4a04d253df0c990125743a24fa8d0818560dd686
James Newell <james.newell@mryum.com>
2023-07-31 13:44:01 +1000

    fix formatting

--- src/builders/Pipeline.test.ts
+++ src/builders/Pipeline.test.ts
@@ -2,31 +2,36 @@ import {Pipeline} from './Pipeline';
 import {CommandStep} from './CommandStep';
 
 describe('Pipeline', () => {
-
   describe('agents', () => {
     test('does not have agents when no agents are setup', () => {
       const pipeline = new Pipeline().build();
-      expect(pipeline).not.toHaveProperty('agents')
-    })
+      expect(pipeline).not.toHaveProperty('agents');
+    });
 
     test('has agents when agents are setup', () => {
       const pipeline = new Pipeline().agent('queue', 'macos').build();
-      expect(pipeline).toHaveProperty('agents', expect.objectContaining({queue: 'macos'}))
-    })
-  })
+      expect(pipeline).toHaveProperty(
+        'agents',
+        expect.objectContaining({queue: 'macos'}),
+      );
+    });
+  });
 
   describe('notifications', () => {
     test('does not have notifications when no notifications are setup', () => {
       const pipeline = new Pipeline().build();
-      expect(pipeline).not.toHaveProperty('notify')
-    })
+      expect(pipeline).not.toHaveProperty('notify');
+    });
 
     test('has notifications when notifications are setup', () => {
-      const notification = {email: 'james@example.com'}
+      const notification = {email: 'james@example.com'};
       const pipeline = new Pipeline().notify(notification).build();
-      expect(pipeline).toHaveProperty('notify', expect.arrayContaining([notification]))
-    })
-  })
+      expect(pipeline).toHaveProperty(
+        'notify',
+        expect.arrayContaining([notification]),
+      );
+    });
+  });
 
   test('has steps', () => {
     const step1 = new CommandStep().command('echo "hello"');
@@ -42,5 +47,4 @@ describe('Pipeline', () => {
       ]),
     );
   });
-
 });
--- src/builders/Pipeline.ts
+++ src/builders/Pipeline.ts
@@ -3,28 +3,27 @@ import {PipelineBuilder} from './PipelineBuilder';
 import {StepBuilder} from './StepBuilder';
 import {isStepBuilder} from './isStepBuilder';
 import {StepsBuilder} from './helpers/steps';
-import { AgentsObject } from '../schema/schema';
-
+import {AgentsObject} from '../schema/schema';
 
 export class Pipeline implements PipelineBuilder, StepsBuilder {
-  #agents: AgentsObject = {}
-  #notify: Array<PipelineNotify> = []
+  #agents: AgentsObject = {};
+  #notify: Array<PipelineNotify> = [];
   #steps: Array<StepSchema | StepBuilder> = [];
 
   /**
    * @see https://buildkite.com/docs/agent/v3/cli-start#agent-targeting
    */
   agent(tag: string, value: string): this {
-    this.#agents[tag] = value
-    return this
+    this.#agents[tag] = value;
+    return this;
   }
 
   /**
    * @see https://buildkite.com/docs/pipelines/notifications
    */
   notify(notify: PipelineNotify): this {
-    this.#notify.push(notify)
-    return this
+    this.#notify.push(notify);
+    return this;
   }
 
   step(step: StepSchema | StepBuilder): this {
@@ -40,11 +39,11 @@ export class Pipeline implements PipelineBuilder, StepsBuilder {
     };
 
     if (this.#notify.length) {
-      pipeline.notify = this.#notify
+      pipeline.notify = this.#notify;
     }
 
     if (Object.keys(this.#agents).length) {
-      pipeline.agents = this.#agents
+      pipeline.agents = this.#agents;
     }
 
     return pipeline;
